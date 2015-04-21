c39fb12541003db213f58aade0c1705da454b960
汪潇翔 <546022398@qq.com>
2015-04-21 17:43:01 +0800

    init CocoaTouchUI

---
+++ CocoaTouch/CocoaTouchUI/CocoaTouchUI/CocoaTouchUI.h
@@ -0,0 +1,19 @@
+//
+//  CocoaTouchUI.h
+//  CocoaTouchUI
+//
+//  Created by 汪潇翔 on 15/4/21.
+//  Copyright (c) 2015年 汪潇翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoaTouchUI.
+FOUNDATION_EXPORT double CocoaTouchUIVersionNumber;
+
+//! Project version string for CocoaTouchUI.
+FOUNDATION_EXPORT const unsigned char CocoaTouchUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaTouchUI/PublicHeader.h>
+
+
---
+++ CocoaTouch/CocoaTouchUI/CocoaTouchUITests/CocoaTouchUITests.m
@@ -0,0 +1,40 @@
+//
+//  CocoaTouchUITests.m
+//  CocoaTouchUITests
+//
+//  Created by 汪潇翔 on 15/4/21.
+//  Copyright (c) 2015年 汪潇翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface CocoaTouchUITests : XCTestCase
+
+@end
+
+@implementation CocoaTouchUITests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
